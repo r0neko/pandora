2c4906bd3b225a364622a84b633825db31930c6e
Alexandru Talnaci <flafyrex@gmail.com>
2022-12-26 20:46:51 +0200

    Created API Sessions migration

---
+++ database/migrations/2022_12_25_201736_create_api_sessions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->string('token', 64)->unique();
+            $table->foreignId('owner_id');
+            $table->string('ip', 64);
+            $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_sessions');
+    }
+};
